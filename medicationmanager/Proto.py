f84e372862683e8c52c9aabc3a191d7f06105d3c
dean.wright <dean@ozplanit.com>
2017-10-28 12:09:47 +0800

    initial load

--- medicationmanager/Proto.py
+++ medicationmanager/Proto.py
@@ -3,27 +3,34 @@
 # Created by: dean.wright
 # Created on: 17/10/2017
 
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from medicationmanager.services.core import Patient, Medication, Contact
+
+# create a person *** change this to a Patient and reuse person for contacts also!!
+person1 = Patient("Dean", "Wright", 45)
+
+# create a contact for the person
+contact1 = Contact("Karen", "22360223@student.uwa.edu.au", "0438749904")
+
+#create first medication
+med = Medication()
+med.dosage = "2 Tablet"
+med.name = "Panadiene Forte"
+med.strength = "500mg"
+med.application = "6hr"
+med.repeats = 5
+med.script = True
+
+#create a second medication
+med2 = Medication()
+med2.dosage = "2 Tablet"
+med2.name = "Panadiene Forte"
+med2.strength = "500mg"
+med2.application = "6hr"
+med2.repeats = 5
+med2.script = True
+
+#add the medications to the person
+person1.medications.append(med)
+person1.medications.append(med2)
 
 
-class Proto(QTWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-
-
-    def initUI(self):
-        self.setGeometry(300, 300, 300, 220)
-        self.setWindowTitle('Icon')
-        self.setWindowIcon(QIcon('web.png'))
-        self.show()
-
-
-    if __name__ == '__main__':
-        app = QApplication(sys.argv)
-        ex = Example()
-        sys.exit(app.exec_())
--- medicationmanager/test/test_services.py
+++ medicationmanager/test/test_services.py
@@ -2,3 +2,50 @@
 # Objective : TODO
 # Created by: dean.wright
 # Created on: 25/10/2017
+
+import datetime
+import time
+import asyncio
+
+from medicationmanager.services.core import DoseAlarm
+
+
+def test_alarm():
+    notificationqueue = asyncio.Queue(10)
+    alarmfor = "Dean"
+    alarmdue = datetime.datetime.now() + datetime.timedelta(minutes=1)
+    alarm = DoseAlarm(alarmfor, alarmdue, notificationqueue)
+    alarm.start()
+    queueisempty = True
+
+    while notificationqueue.empty():
+        print("Alarm notifciation queue is empty " + datetime.datetime.now().ctime() + " is the current time.")
+        time.sleep(5)
+
+    print("Alarm notification for " + notificationqueue.get())
+
+
+test_alarm()
+
+
+
+async def print_every_second():
+    "Print seconds"
+    while True:
+        for i in range(60):
+            print(i, 's')
+            await asyncio.sleep(1)
+
+
+async def print_every_minute():
+    for i in range(1, 10):
+        await asyncio.sleep(60)
+        print(i, 'minute')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    asyncio.gather(print_every_second(),
+                   print_every_minute())
+)
+loop.close()
