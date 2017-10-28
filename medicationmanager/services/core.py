5ee35d2f5cac1f2cb053b8bd2bda2b711e52ba90
dean.wright <dean@ozplanit.com>
2017-10-28 12:09:05 +0800

    added objects

--- medicationmanager/services/core.py
+++ medicationmanager/services/core.py
@@ -2,17 +2,30 @@
 # Objective : TODO
 # Created by: dean.wright
 # Created on: 25/10/2017
+import asyncio
+import threading
+import time
+import datetime
 
-class Person(object):
+
+class Patient(object):
     def __init__(self, firstname, lastname, age):
         # The persons identifying attributes
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
+        self.email = None
+        self.phone = None
         # A collection of Medications being used by the Person
         self.medications = []
         # A collection of contacts the person would like to notify of medication problems
         self.contacts = []
+
+class Contact(object):
+    def __init__(self, name, email, sms):
+        self.name = name
+        self.email = email
+        self.sms = sms
 
 
 class Medication(object):
@@ -30,9 +43,51 @@ class Medication(object):
         # how many repeats on the script if this a prescription medication
         self.repeats = None
 
-class DoseAlarm(object):
-    def __init__(self, medications):
-        self.medications = medications
+
+class DoseAlarm(threading.Thread):
+    def __init__(self, alarmfor, datetime, notify):
+        super(DoseAlarm, self).__init__()
+        self.alarmfor = alarmfor
+        self.datetime = datetime
+        self.notify = notify
+
+    def run(self):
+        try:
+            currenttime = datetime.datetime.now()
+            timetoalarm = (self.datetime-currenttime).total_seconds()
+            print(timetoalarm)
+            time.sleep(timetoalarm)
+            self.notify.put(self.alarmfor)
+            return
+        except Exception as exc:
+            raise exc
 
 
+class MedicationManager(object):
+    def __init(self):
+        # patients to manage
+        self.patients = []
+        #get the notification alarm notification queue ready
+        self.alarms = asyncio.Queue()
 
+        self.setup_alarms(self)
+        #notification manager takes care of intereactions with the patient once the dosage due alarm has been raised
+        self.notification_manager = NotificationManager()
+
+        #load any existing patients
+
+    def setup_alarms(self):
+        self.alarms
+        pass
+
+    def add_patient(self, new_patient):
+        #todo future version should check patient not already added
+        self.patients.append(new_patient)
+
+    def remove_patient(self, existing_patient):
+        #todo if patient not in list raise patient not_found_error
+        self.patients.remove(existing_patient)
+
+
+class NotificationManager(object):
+    pass
